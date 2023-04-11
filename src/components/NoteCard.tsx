85259cac6bca86fdd68137cd45db64ed15f4513d
Diego Felipe Hoyos Alog <dhoyos@alogistica.co>
2023-04-10 23:33:00 -0500

    EL mega note taker 300 casi listo yeah

---
+++ src/components/NoteCard.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import { type RouterOutputs } from "y/utils/api";
+
+type Note = RouterOutputs["note"]["getAll"][0];
+
+export const NoteCard = ({
+  note,
+  onDelete,
+}: {
+  note: Note;
+  onDelete: () => void;
+}) => {
+  const [isExpanded, setIsExpanded] = useState<boolean>(true);
+
+  return (
+    <div className="card mt-5 border border-gray-200 bg-base-100 shadow-xl">
+      <div className="card-body m-0 p-3">
+        <div
+          className={`collapse-arrow ${
+            isExpanded ? "collapse-open" : ""
+          } collapse`}
+          onClick={() => setIsExpanded(!isExpanded)}
+        >
+          <div className="text-xlfont-bold collapse-title">{note.title}</div>
+          <div className="collapse-content">
+            <article className="prose lg:prose-xl">
+              <ReactMarkdown>{note.content}</ReactMarkdown>
+            </article>
+          </div>
+        </div>
+        <div className="card-actions mx-2 flex justify-end">
+          <button className="btn-warning btn-xs btn px-5" onClick={onDelete}>
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
